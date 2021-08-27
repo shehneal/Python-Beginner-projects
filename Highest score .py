c26cc70a129e91e2d251324558e3857f8c641f8c
shehneal <80928253+shehneal@users.noreply.github.com>
2021-08-27 14:11:40 -0700

    Add files via upload
    without using max() function

---
+++ Highest score .py
@@ -0,0 +1,12 @@
+student_scores = input("Input a list of student scores ").split()
+max = student_scores[0]
+for score in student_scores:
+  if score > max:
+    max = score 
+    print(max)
+
+    
+
+
+
+
