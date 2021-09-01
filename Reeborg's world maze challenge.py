7b85fbe76fdc06882fc59bd4f572430a74f59395
shehneal <80928253+shehneal@users.noreply.github.com>
2021-09-01 13:37:24 -0700

    Add files via upload

---
+++ Reeborg's world maze challenge.py
@@ -0,0 +1,27 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal():
+     turn_right
+     if front_is_clear():
+         move()
+     if right_is_clear():
+         turn_right()
+     elif wall_in_front() and wall_on_right():
+         turn_left()
+            
+    
+   
+    
+    
+        
+    
+        
+
+    
+   
+    
+        
+ 
+        
