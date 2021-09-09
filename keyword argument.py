c03000bfd41864279c499bf0b5372b0392483367
shehneal <80928253+shehneal@users.noreply.github.com>
2021-09-09 12:05:22 -0700

    Add files via upload

---
+++ keyword argument.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet(name, location):
+  print(f"Hello! {name}")
+  print("How are you?")
+  print(f"Weather here in {location} is so good.")
+
+greet(location = "California", name = "Jill")
+
